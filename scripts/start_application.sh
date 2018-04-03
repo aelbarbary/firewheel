0b8d6d558d95cdd62337eb10ace0e0c34d47e8b8
aelbarbary <abdelrahman.elbarbary@gmail.com>
2018-04-03 09:10:09 -0700

    code deploy

--- scripts/start_application.sh
+++ scripts/start_application.sh
@@ -1,7 +1,6 @@
 # sudo python3 /home/ubuntu/www/tamkeen/app/manage.py runserver 0.0.0.0:80 > /dev/null 2>&1 &
-sudo systemctl restart nginx
-
 sudo kill -9 $(sudo lsof -t -i:8004)
 sudo uwsgi --chdir /home/ubuntu/www/larayb/backend --socket :8004 --module backend.wsgi > /dev/null 2>&1 &
-
 yarn run build --prefix /home/ubuntu/www/larayb/frontend
+
+sudo systemctl restart nginx
