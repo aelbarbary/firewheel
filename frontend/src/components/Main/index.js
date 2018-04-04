c909efb2c97817bd5984da80dbfbfb7e030455fb
aelbarbary <abdelrahman.elbarbary@gmail.com>
2018-04-04 11:56:14 -0700

    use random user generator

--- frontend/src/components/Main/index.js
+++ frontend/src/components/Main/index.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './styles.sass';
 import Item from '../Item/index';
-import Background from '../Background/index'
 
 class Homepage extends Component {
 
