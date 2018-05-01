8c3c7420ca014dfcc4968e09528d6fa9282ee289
aelbarbary <abdelrahman.elbarbary@gmail.com>
2018-05-01 08:29:17 -0700

    admin static files

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -182,7 +182,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-# STATIC_ROOT = '/static/'
+STATIC_ROOT = '/home/ubuntu/www/larayb/backend/static'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
