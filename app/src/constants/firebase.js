13b0dd93e85b8917c20d50800f28d7e6fb08a1ae
aelbarbary <abdelrahman.elbarbary@gmail.com>
2018-06-19 16:08:20 -0700

    setup firebase

--- app/src/constants/firebase.js
+++ app/src/constants/firebase.js
@@ -1,8 +1,8 @@
 export default {
-  apiKey: 'AIzaSyAHshJehJ-E_9AoUN3kfY8BLvZtFoXxFto',
-  authDomain: 'react-native-starter-app.firebaseapp.com',
-  databaseURL: 'https://react-native-starter-app.firebaseio.com',
-  projectId: 'react-native-starter-app',
-  storageBucket: 'react-native-starter-app.appspot.com',
-  messagingSenderId: '762822550667',
+  apiKey: 'AIzaSyDq6slRob2-u0ovhSyV0lm57EaZeUq8fhc',
+  authDomain: 'larayb-4935f.firebaseapp.com',
+  databaseURL: 'https://larayb-4935f.firebaseio.com/',
+  projectId: 'larayb-4935f',
+  storageBucket: 'larayb-4935f.appspot.com',
+  messagingSenderId: '910645061298',
 };
