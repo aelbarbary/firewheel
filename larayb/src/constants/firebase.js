7ba78ffc309519821e4a108c39b68bb7de1f79ed
Abdel <abdelrahman.elbarbary@gmail.com>
2018-07-03 15:09:36 -0700

    remove exiting code

--- larayb/src/constants/firebase.js
+++
@@ -1,8 +0,0 @@
-export default {
-  apiKey : 'AIzaSyDq6slRob2-u0ovhSyV0lm57EaZeUq8fhc',
-  authDomain : 'larayb-4935f.firebaseapp.com',
-  databaseURL : 'https://larayb-4935f.firebaseio.com/',
-  projectId : 'larayb-4935f',
-  storageBucket : 'larayb-4935f.appspot.com',
-  messagingSenderId : '910645061298'
-};
