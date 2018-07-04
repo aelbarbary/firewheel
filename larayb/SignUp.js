b60a425ad9162104fdd839c8c074548c52bc973a
Abdel <abdelrahman.elbarbary@gmail.com>
2018-07-04 14:50:06 -0700

    style sign up page

--- larayb/SignUp.js
+++ larayb/SignUp.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Button, StyleSheet, View, Text, Alert} from 'react-native'
+import {Button, StyleSheet, View, Text, Alert, Image} from 'react-native'
 import t from 'tcomb-form-native';
 
 const Form = t.form.Form;
@@ -29,6 +29,11 @@ class SignUp extends React.Component {
     return (
 
       <View style={styles.container}>
+        <View style={styles.logo}>
+          <Image source={require('./logo.png')} style={{width:100, height:100, }} />
+          <Text>LARAYB</Text>
+        </View>
+
         <Text>{this.props.emai} </Text>
         <Form type={User} ref={ c => this._form = c }/>
         <Button title="Sign up" onPress={this.handleSubmit}/>
@@ -44,6 +49,11 @@ const styles = StyleSheet.create({
     padding: 20,
     backgroundColor: '#ffffff',
   },
+  logo:{
+    alignItems: 'center',
+    padding:10
+  }
 });
+
 
 export default SignUp ;
